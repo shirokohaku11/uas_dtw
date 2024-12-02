21a5b2a8f4ae3730de32c9548a991f13f6ae969c
silver <86962442+shirokohaku11@users.noreply.github.com>
2024-12-02 23:53:48 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,14 +2,10 @@ window.onload = function() {
 	const preloader = document.getElementById('preloader');
 	const mainContent = document.getElementById('main-content');
   
-	// Tampilkan preloader selama 2 detik
 	setTimeout(() => {
-	  // Hilangkan preloader
 	  preloader.classList.add('hidden');
-  
-	  // Tampilkan halaman utama
 	  mainContent.classList.remove('hidden');
 	  mainContent.classList.add('visible');
-	}, 2000); // Durasi preloader dalam milidetik
+	}, 2000);
   };
   
